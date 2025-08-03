0c9a87429411052a5e7253fbcc98f2331b7d690e
paullooll <richieaquino56@gmail.com>
2025-08-04 00:02:24 +0800

    fix hero section

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Richie Paul
         </motion.p>
         <div>
           <motion.p
