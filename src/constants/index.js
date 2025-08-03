b36cdc82e5a2b6cdfd4c8bec0dc560b2cb50e80a
paullooll <richieaquino56@gmail.com>
2025-08-03 23:16:16 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,139 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "ChorDefine",
+    description:
+      "A Mobile App for guitar chord practice and training using computer vision and audio recognition.",
+    subDescription: [
+      "Detect frequency from audio input for multi-modal evaluation using the Yin algorithm to classify guitar chords",
+      "Utilize CNN and machine learning to identify the position of the guitarist's fingers and hand on the fretboard that prompts if the position is incorrect.",
+      "Allow users to select songs that match their interests and skill level.",
+      "Track the user’s progress to support independent practice.",
+    ],
+    href: "https://github.com/ronmagpantay1/ChorDefine",
+    logo: "",
+    image: "/assets/projects/3.png",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/flutter.png",
+      },
+      {
+        id: 2,
+        name: "Google Colab",
+        path: "/assets/logos/colab.png",
+      },
+      {
+        id: 3,
+        name: "Dart",
+        path: "/assets/logos/dart.png",
+      },
+      {
+        id: 4,
+        name: "Roboflow",
+        path: "/assets/logos/robo.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Face Recognition Attendance Scanner",
+    description:
+      "The Face Recognition Attendance Scanner uses computer vision to automate attendance tracking.",
+    subDescription: [
+      "It identifies individuals by analyzing unique facial features.",
+      "Designed for schools, offices, and events, it enhances security.",
+      "The system ensures fast, accurate, and hygienic check-ins.",
+    ],
+    href: "https://github.com/paullooll/FaceRecognitionAttendanceSystem",
+    logo: "",
+    image: "/assets/projects/1.png",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: "MS Access",
+        path: "/assets/logos/access.png",
+      },
+      {
+        id: 3,
+        name: "Haar Cascade",
+        path: "/assets/logos/cv.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Ambatu.Help",
+    description:
+      "Intelligent chatbot powered by the ChatGPT API, designed to deliver instant, accurate, and conversational support across various topics.",
+    subDescription: [
+      "It leverages advanced natural language understanding to assist users with queries, guide through tasks, and provide personalized responses.",
+      "Available on smartphones, it helps users get instant answers, and access support through natural, intuitive chats.",
+      "With a user-friendly interface and powerful AI under the hood, Ambatu.Help is your personal assistant in your pocket.",
+    ],
+    href: "https://github.com/paullooll/cc206_chatbot_application",
+    logo: "",
+    image: "/assets/projects/2.png",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/flutter.png",
+      },
+      {
+        id: 2,
+        name: "Dart",
+        path: "/assets/logos/dart.png",
+      },
+      {
+        id: 3,
+        name: "TensorFlow",
+        path: "/assets/logos/tf.png",
+      },
+      {
+        id: 4,
+        name: "ChatGPT API",
+        path: "/assets/logos/chat.png",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/richiepaul.lagamonaquino",
+    icon: "/assets/socials/fb.webp",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/richie-paul-aquiño-2bb196265",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ret.cheee",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer Intern",
+    job: "Fullstack Developer",
+    date: "Jan - April 2025",
+    contents: [
+      "Assisted in creating the Frontend Web of The Prominent Project using Next.js and Tailwind CSS.",
+      "Conducted Quality Assurance testing for The Prominent Project.",
+      "Created the Authentication of project Spaceport using JWT Authentication.",
+      "Assisted in setting up the PostgreSQL database for the project Spaceport.",
+      "Adapted quickly to new tools, frameworks, and workflows to support evolving project demands and team needs.",
+    ],
+  },
+];
